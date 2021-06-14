0beaa1fd9eba37d09e0e95eb6547b8f0f30ce873
kammalage <shanantonfernando@gmail.com>
2021-06-13 21:30:19 -0700

    created simple web server test

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", testHandler)
+	http.Handle("/", r)
+	fmt.Println("Starting up on 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("Hello World!")
 }
