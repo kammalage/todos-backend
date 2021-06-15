20decf94c66a182c44454b25d84f824b77baaced
kammalage <shanantonfernando@gmail.com>
2021-06-14 22:57:14 -0700

    added handlers with test response

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -11,13 +12,26 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", testHandler)
+	r.HandleFunc("/", listTodosHandler).Methods("GET")
+	r.HandleFunc("/add", createTodosHandler).Methods("POST")
+	r.HandleFunc("/delete", deleteTodosHandler).Methods("DELETE")
 	http.Handle("/", r)
 	fmt.Println("Starting up on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
+func listTodosHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"test" : true}`)
+	fmt.Println("listTodosHandler")
+}
+
+func createTodosHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Println("Hello World!")
+	fmt.Println("createTodosHandler")
+}
+
+func deleteTodosHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("deleteTodosHandler")
 }
